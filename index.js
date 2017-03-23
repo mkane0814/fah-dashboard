2abe91d6ab6f84d268c8219d7fa2e0e2ebc24d69
Dubois Grayson <Grayson.Dubois@hcahealthcare.com>
2017-03-22 23:38:13 -0500

    Added dependencies: d3, vue, express, async
    Began refactoring index.js to house main server
    
    Added simple child_process logic that spawns a thread to handle the download of the data

--- index.js
+++ index.js
@@ -1,90 +1,25 @@
 /**
- Latest edit : Matt Kane March 16 at 4:44 pm
+ Latest edit: Grayson Dubois on March 22
  */
  /*jshint esversion: 6*/
-const cron = require("node-cron");
 const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const child_process = require("child_process");
 const MongoClient = require("mongodb").MongoClient;
-const Stream = require("stream");
-const readline = require("readline");
-const url = "http://fah-web.stanford.edu/daily_user_summary.txt";
-const dlDIR = "./DataFiles/";
 
-let versionNumber = 0;
-
-// Checking if there is a DataFiles directory.
-// If not then create one.
-fs.stat(dlDIR, function (err){
-    if(err) {
-        console.log("DataFiles directory does not exist. Creating " + dlDIR);
-        fs.mkdir(dlDIR);
-    }
+// Fork a separate process to handle the data download and parsing
+var downloadTask = child_process.exec("node app14.js", function(error, stdout, stderr) {
+	if (error) {
+		console.log(error.stack);
+		console.log("Error code: " + error.code);
+		console.log("Signal recieved: " + error.signal);
+		console.log("app14.js stderr: " + stderr);
+	}
+	if (stdout) {
+		console.log("app14.js stdout: " + stdout);
+	}
 });
 
-function job() {
-    //Connect to the Mongo Database
-    MongoClient.connect("mongodb://localhost:27017/userDBTest", function (err, db) {
-        if (err) return console.dir(err);
-        console.log("We are connected");
-
-        let collection = db.collection('data');
-        console.log("Downloading Data...");
-
-        // Create the filestream before making the http request
-        let file = fs.createWriteStream(dlDIR +"downloaded-file-" + versionNumber +".txt");
-
-        // Download data
-        let request = http.get(url, function(response) {
-            if (response.statusCode === 200) {
-                response.pipe(file);
-                
-                file.on("finish", function() {
-                    file.close();
-                    console.log("Successfully downloaded file");
-
-                    //Import to Test DB
-                    let instream = fs.createReadStream(dlDIR +"downloaded-file-" + versionNumber +".txt");
-                    let outstream = new Stream();
-                    let date;
-                    let rl = readline.createInterface(instream, outstream);
-    
-                    //Get the timestamp from the first line of the data file
-                    rl.once("line", function (line) {
-                        date = line;
-                    });
-        
-                     // One thing I forgot, the way it is now the first line that is read by this function gets junk.
-                    // Not sure how to skip that one line.
-                    rl.on("line", function(line) {
-                        //console.log(line);
-                        let stringArr = line.split("\t");
-                        collection.insert({timeStamp : date, name: stringArr[0], newCredits : stringArr[1], sumTotal : stringArr[2], team : stringArr[3]}, {w: 1});
-                    });
- 
-                   rl.on("close", function () {
-                        versionNumber++;
-                    });
-                });
-
-            }
-
-            request.setTimeout(12000, function() {
-                console.log("Failed to download data: connection timed out");
-                request.abort();
-            });
-        }).on("error", function(err) {
-            console.log("Error making http request: " + err);
-        }); 
-
-	    
-    });
-}
-
-job();
-
-//call the job on the 0th minute of every hour
-/*cron.schedule('0 * * * *', function() {
-    // Call the job
-    job();
-});*/
+downloadTask.on("exit", function(exitCode) {
+	console.log("app14.js exited with code " + exitCode);
+});
