9d6f007f890cab9443a7fa85a7a33e9647886917
Matt Kane <kanemtthw@gmail.com>
2017-03-24 17:53:03 -0500

    Stil messing with stuff

--- backendAPI.js
+++ backendAPI.js
@@ -1,11 +1,15 @@
 /* jshint esversion : 6*/
 
-var express = require("express");
-var mongoOP = require("mongoConnect");
-var app = express();
+var Vue = require("vue");
+var vueRouter = require("vue-router");
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var express = require("express");
+var app = express();
 var router = express.Router();
 
+
+mongoose.connect("mongodb://localhost:27017/userTestDB");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
@@ -13,26 +17,31 @@ router.get("/", function(req, res){
 	res.json({"error" : false, "message" : "Hello World"});
 });
 
-router.route("/users")
-	.get(function (req, res){
-		var response = {};
 
-		mongoOP.find({/* SEARCH PARAM */}, function(err, data){
-			if(err){
-				response = {"error" : true, "message" : "ERROR FETCHING DATA"};
-			} else {
-				response = {"error" : false, "message" : data};
-			}
+function getReqUser(params)
+{
+	router.route("/users")
+		.get(function (req, res){
+			var response = {};
 
-			res.json(response);
+			mongoose.find({params}, function(err, data){
+				if(err){
+					response = {"error" : true, "message" : "ERROR FETCHING DATA"};
+				} else {
+					response = {"error" : false, "message" : data};
+				}
+
+				res.json(response);
 		});
 	});
+}
 
+function getReqTeam(params){
 router.route("/teams")
 	.get(function (req, res){
 		var response = {};
 
-		mongoOP.find({/* SEARCH PARAM */}, function(err, data){
+		mongoose.find({/* SEARCH PARAM */}, function(err, data){
 			if(err){
 				response = {"error" : true, "message" : "ERROR FETCHING DATA"};
 			} else {
@@ -43,8 +52,44 @@ router.route("/teams")
 		});
 	});
 
+}
 
-app.use('/', router);
+var Teams = {
+	template: "<div>/teams</div>"
+};
+var Users = {
+	template: `
+		<div class="users">
+			<h2>User {{route.params.id}}</h2>
+			<router-view></router-view>
+		</div>
+	`
 
-app.listen(3000);
-console.log("Listening on PORT 3000");
+};
+
+var vrouter = new vueRouter({
+	routes: [
+		{ path : "/users/:id", component: Users,
+		children: [
+			{
+				path: 'userview',
+				component: UserView
+			},
+
+		]},
+		{ path : "/teams/:id", component: Teams,
+		children: [
+			{
+				path: 'teamview',
+				component: TeamView
+			}
+		]}
+	]
+});
+
+var vm = new Vue({
+	el: "#user-element",
+	message: getReqUser(/*Search Param*/)
+
+	
+});
