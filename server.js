e6104d44fa1116716557921b9815b9da3ac61c75
Matt Kane <kanemtthw@gmail.com>
2017-03-30 13:21:04 -0500

    Updated apt.get function to send the proper response

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
  Latest edit: Troy Herbison on March 28
  */
 
+ /* jshint esversion: 6 */
+
 const url_db = 'mongodb://localhost:27017/folding';
 var app = require('express')();
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
 // Use connect method to connect to the Server
@@ -19,23 +21,22 @@ MongoClient.connect(url_db, function(err, db) {
     	var limit = req.params.limit;
 
     	//query top teams or users then send a response as an object
-    	queryDB(userOrTeam, limit, obj);
-    	res.send(obj);
+    	res.send(queryDB(userOrTeam, limit, obj)); 
+    	
     	
 	});
 
 	//can uncomment below to test function
-	//queryDB('teams', 100, obj);
+	queryDB('teams', 100, obj);
 
 	function queryDB(teamOrUser, limNum, obj)
 	{
-		db.collection(teamOrUser).find().limit(limNum).sort({ score : -1 }).toArray(function(err, obj) {
+		return db.collection(teamOrUser).find().limit(limNum).sort({ score : -1 }).toArray(function(err, obj) {
 			if (err) return console.log(err.message);
 
 			//obj contains the queried contents. Can uncomment below to test
-			//console.log(obj); 
-			db.close();
-		
+			console.log(obj); 
+			//db.close();
 		});
 	}
 
