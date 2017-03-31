ef1ba1f4867216b1ee7fb7d266e642e73dd91b39
Troy Herbison <herbison3236@gmail.com>
2017-03-30 22:15:27 -0500

    Added find capabilities and more sort options

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /**
- Latest edit: Troy Herbison on March 28
+ Latest edit: Troy Herbison on March 30
  */
 
  /* jshint esversion: 6 */
@@ -17,25 +17,65 @@ MongoClient.connect(url_db, function(err, db) {
   	var userOrTeam;
   	var limit;
 
-  	
-  		//send response if there is a request in this directory
-		app.get('/:limit/:userOrTeam', function(req, res) {
-    		var userOrTeam = req.params.userOrTeam;
-    		var limit = req.params.limit; 
-    		//query top teams or users then send a respons1e as an object
-    		
+  	//send response if user wants to sort by rank, score, units, or changes in these values
+	app.get('/sort/:limit/:userOrTeam/:sortVal', function(req, res) {
+		userOrTeam = req.params.userOrTeam;
+    	limit = parseInt(req.params.limit);
+    	
+		var sortBy = req.params.sortVal;
+    	
+    	//query top teams or users then send a response as an object
+    	db.collection(userOrTeam).find().limit(limit).sort({ sortBy : -1 }).toArray(function(err, obj) {
+			if (err) return console.log(err.message);
 
-    		b.collection(teamOrUser).find().limit(limNum).sort({ score : -1 }).toArray(function(err, obj) {
+			res.send(obj);
+		});
+	});
+
+	//send response if user wants to search for name, teamID, or rank
+	app.get('/find/:findVal/:findField/:userOrTeam', function(req, res) {
+		userOrTeam = req.params.userOrTeam;
+
+		//findVal could be a name, teamID, or rank
+		var findVal = req.params.findVal;
+		var findField = req.params.findField.toLowerCase();
+
+		//should remove daily and hourly json fields before sending object here
+		//delete obj["hourly"]; ***doesn't work
+
+		if(findField == "name")
+		{
+			//search for name
+	    	db.collection(userOrTeam).find({"_id.name" : findVal}).toArray(function(err, obj) {
 				if (err) return console.log(err.message);
-				console.log(obj);
+				
 				res.send(obj);
 			});
-		});
+    	}
+    	else if(findField == "teamid")
+    	{
+    		//search for teamID
+    		db.collection(userOrTeam).find({"_id.teamID" : findVal}).toArray(function(err, obj) {
+				if (err) return console.log(err.message);
+				
+				res.send(obj);
+			});
+    	}
+    	else if(findField == "rank")
+    	{
+    		//search for rank
+    		db.collection(userOrTeam).find({rank : parseInt(findVal)}).toArray(function(err, obj) {
+				if (err) return console.log(err.message);
+				
+				res.send(obj);
+			});
+    	}
+
+	});
 	
-	//can uncomment below to test function
-	
-	var client = http.client();
-	client.get('/:limit/:userOrTeam', function ())
+
+	//var client = http.client();
+	//client.get('/:limit/:userOrTeam', function (){ });
 
 	app.listen(3000, function (){
 		console.log("Listening on port 3000");
